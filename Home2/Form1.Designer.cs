a4d1afe727c29dd76ea4a8a9ee61c46a509e6afb
gfghrbdf <91376630+gfghrbdf@users.noreply.github.com>
2022-02-28 21:37:13 +0600

    hi

--- Home2/Form1.Designer.cs
+++ Home2/Form1.Designer.cs
@@ -249,6 +249,7 @@
             this.button5.TabIndex = 17;
             this.button5.Text = "Sign In";
             this.button5.UseVisualStyleBackColor = false;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
             // 
             // Form1
             // 
